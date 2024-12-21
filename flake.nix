3363c1fb927273ecad3ce37e7f39b3cb14c3e266
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2024-12-21 13:14:34 -0500

    trying to package it aint working. bruh :(

--- flake.nix
+++ flake.nix
@@ -2,8 +2,12 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+
     rust-overlay.url = "github:oxalica/rust-overlay";
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+
+    pyproject-nix.url = "github:pyproject-nix/pyproject.nix";
+    pyproject-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
@@ -11,6 +15,7 @@
     nixpkgs,
     flake-utils,
     rust-overlay,
+    pyproject-nix,
   } @ inputs:
     flake-utils.lib.eachDefaultSystem
     (
@@ -18,18 +23,45 @@
         overlays = [(import rust-overlay)];
         pkgs = import nixpkgs {inherit system overlays;};
 
-        rustPkg = pkgs.rust-bin.stable.latest.default;
-        pythonPkg = pkgs.python3;
-      in {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [alejandra maturin pythonPkg rustPkg];
-
-          shellHook = ''
-            export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:${pkgs.pythonManylinuxPackages.manylinux2014Package}/lib:$LD_LIBRARY_PATH";
-            test -d .nix-venv || ${pythonPkg.interpreter} -m venv .nix-venv
-            source .nix-venv/bin/activate
-          '';
+        project = pyproject-nix.lib.project.loadPyproject {
+          projectRoot = ./.;
         };
+
+        rust = pkgs.rust-bin.stable.latest.default;
+        python = pkgs.python3.override {
+          self = python;
+          packageOverrides = pyfinal: pyprev: {
+            inherit (pkgs) maturin;
+            typer-slim = pyfinal.typer;
+          };
+        };
+      in {
+        devShells.default = let
+          pythonEnv = python;
+          /*
+                              assert project.validators.validateVersionConstraints {inherit python;} == {};
+          python.withPackages (project.renderers.withPackages {inherit python;});
+          */
+        in
+          pkgs.mkShell {
+            buildInputs = with pkgs; [alejandra maturin rust pythonEnv];
+
+            shellHook = ''
+              export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:${pkgs.pythonManylinuxPackages.manylinux2014Package}/lib:$LD_LIBRARY_PATH";
+              test -d .nix-venv || ${python.interpreter} -m venv .nix-venv
+              source .nix-venv/bin/activate
+            '';
+          };
+
+        packages.default = let
+          # Returns an attribute set that can be passed to `buildPythonPackage`.
+          attrs = project.renderers.buildPythonPackage {
+            inherit python;
+          };
+        in
+          # Pass attributes to buildPythonPackage.
+          # Here is a good spot to add on any missing or custom attributes.
+          python.pkgs.buildPythonPackage attrs;
       }
     );
 }
