1d0bebbc62198b8e01623f29fc3ba7e0a58d09eb
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2024-12-21 13:40:42 -0500

    use native nixpkgs stuff bc its better supported (still not workign)

--- flake.nix
+++ flake.nix
@@ -5,9 +5,6 @@
 
     rust-overlay.url = "github:oxalica/rust-overlay";
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
-
-    pyproject-nix.url = "github:pyproject-nix/pyproject.nix";
-    pyproject-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
@@ -15,7 +12,6 @@
     nixpkgs,
     flake-utils,
     rust-overlay,
-    pyproject-nix,
   } @ inputs:
     flake-utils.lib.eachDefaultSystem
     (
@@ -23,45 +19,41 @@
         overlays = [(import rust-overlay)];
         pkgs = import nixpkgs {inherit system overlays;};
 
-        project = pyproject-nix.lib.project.loadPyproject {
-          projectRoot = ./.;
-        };
-
+        python = pkgs.python3;
         rust = pkgs.rust-bin.stable.latest.default;
-        python = pkgs.python3.override {
-          self = python;
-          packageOverrides = pyfinal: pyprev: {
-            inherit (pkgs) maturin;
-            typer-slim = pyfinal.typer;
-          };
+        rustPlatform = pkgs.makeRustPlatform {
+          rustc = rust;
+          cargo = rust;
         };
       in {
-        devShells.default = let
-          pythonEnv = python;
-          /*
-                              assert project.validators.validateVersionConstraints {inherit python;} == {};
-          python.withPackages (project.renderers.withPackages {inherit python;});
-          */
-        in
-          pkgs.mkShell {
-            buildInputs = with pkgs; [alejandra maturin rust pythonEnv];
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [alejandra maturin rust python];
 
-            shellHook = ''
-              export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:${pkgs.pythonManylinuxPackages.manylinux2014Package}/lib:$LD_LIBRARY_PATH";
-              test -d .nix-venv || ${python.interpreter} -m venv .nix-venv
-              source .nix-venv/bin/activate
-            '';
+          shellHook = ''
+            export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:${pkgs.pythonManylinuxPackages.manylinux2014Package}/lib:$LD_LIBRARY_PATH";
+            test -d .nix-venv || ${python.interpreter} -m venv .nix-venv
+            source .nix-venv/bin/activate
+          '';
+        };
+
+        packages.default = python.pkgs.buildPythonApplication rec {
+          name = "wakapi-anyide-${version}";
+          version = "0.6.8";
+          pyproject = true;
+
+          cargoDeps = rustPlatform.fetchCargoTarball {
+            inherit src;
+            hash = "sha256-qSU1QkYeGrVqWo+H+nB0DziJTjagaPczQhONV6ZFX14=";
           };
 
-        packages.default = let
-          # Returns an attribute set that can be passed to `buildPythonPackage`.
-          attrs = project.renderers.buildPythonPackage {
-            inherit python;
-          };
-        in
-          # Pass attributes to buildPythonPackage.
-          # Here is a good spot to add on any missing or custom attributes.
-          python.pkgs.buildPythonPackage attrs;
+          nativeBuildInputs = with pkgs; [
+            maturin
+            rustPlatform.cargoSetupHook
+            rustPlatform.maturinBuildHook
+          ];
+
+          src = ./.;
+        };
       }
     );
 }
