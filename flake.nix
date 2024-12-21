0afd508510e0bb5e94412fa971c4096d2aa61347
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2024-12-21 12:04:54 -0500

    add venv to allow for installing via maturin develop

--- flake.nix
+++ flake.nix
@@ -17,9 +17,18 @@
       system: let
         overlays = [(import rust-overlay)];
         pkgs = import nixpkgs {inherit system overlays;};
+
+        rustPkg = pkgs.rust-bin.stable.latest.default;
+        pythonPkg = pkgs.python3;
       in {
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [alejandra python313 rust-bin.stable.latest.default];
+          buildInputs = with pkgs; [alejandra maturin pythonPkg rustPkg];
+
+          shellHook = ''
+            export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:${pkgs.pythonManylinuxPackages.manylinux2014Package}/lib:$LD_LIBRARY_PATH";
+            test -d .nix-venv || ${pythonPkg.interpreter} -m venv .nix-venv
+            source .nix-venv/bin/activate
+          '';
         };
       }
     );
