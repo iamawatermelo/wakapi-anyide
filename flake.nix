957f2994d29061e08207273f7c6562601ee90e1b
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2024-12-21 11:41:54 -0500

    add devshell

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {inherit system overlays;};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [alejandra python313 rust-bin.stable.latest.default];
+        };
+      }
+    );
+}
